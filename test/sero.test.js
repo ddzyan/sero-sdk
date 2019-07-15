ecf48c3a9313c4328581b7491a8825f53f8791f2
dingdz <ddzyan@163.com>
2019-07-15 16:22:48 +0800

    add web3

--- test/sero.test.js
+++ test/sero.test.js
@@ -1,9 +1,14 @@
 const assert = require('assert');
-const { Account, Block, Transfer } = require('../lib');
+const SeroSdk = require('../lib');
 
 describe('sero sdk should work', function() {
+  before(function() {
+    const seroSdk = new SeroSdk('http://172.31.225.20:53716');
+    Object.assign(this, seroSdk);
+  });
   it('createAccount', () => {
-    const result = Account.createAccount();
+    const result = this.account.createAccount();
+    console.log(result);
     const { sk, tk_hex, pk, tk_base58, pk_base58 } = result;
     assert(sk, 'sk does not exist');
     assert(tk_hex, 'tk_hex does not exist');
@@ -11,5 +16,14 @@ describe('sero sdk should work', function() {
     assert(tk_base58, 'tk_base58 does not exist');
     assert(tk_base58, 'tk_base58 does not exist');
     assert(pk_base58, 'pk_base58 does not exist');
+  });
+
+  it('getBalance', async () => {
+    try {
+      const balance = await this.account.getBalance('123456');
+      assert(Number.parseInt(balance) > 0, 'balance error');
+    } catch (error) {
+      assert(false, error);
+    }
   });
 });
