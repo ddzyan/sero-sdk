2c9150e5293fbcd025982be6cb6558edeb7fb6a5
ddz <ddzyan@163.com>
2019-07-18 15:34:00 +0800

    修复bug

--- test/sero.test.js
+++ test/sero.test.js
@@ -149,11 +149,8 @@ describe('sero sdk should work', function() {
 
   it('getTransactionReceipt', async () => {
     try {
-      //const bufferSignTx = Buffer.from(signTx, 'utf-8');
-      //const hexSignTx = bufferSignTx.toString('hex');
-      //console.log('hexSignTx',hexSignTx);
       const result = await seroSdk.transfer.getTransactionReceipt(
-        JSON.parse('0x2264b9b0a847796148aec6fbdb94e0dbbe4b57a0207cd2f5318ec1228f2d7e36')
+        '0x2264b9b0a847796148aec6fbdb94e0dbbe4b57a0207cd2f5318ec1228f2d7e36'
       );
       assert.equal(
         result.result.transactionHash,
